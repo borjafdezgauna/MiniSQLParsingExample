7ccfbd335468158744605fe826f292fbde247585
Borja Fernández Gauna <borja.fernandez@ehu.eus>
2021-05-10 10:19:43 +0200

    Added simple server/client example

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                Console.WriteLine("Client running. Waiting 2 seconds...");
+                Task.Delay(2000).Wait();
+
+                Console.Write("Connecting");
+                client.Connect("127.0.0.1", 1200);
+                Console.WriteLine("Connected");
+                Console.WriteLine("Write a message:");
+
+                string msg = Console.ReadLine();
+                NetworkStream stream = client.GetStream();
+
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte [] bytes = encoding.GetBytes(msg);
+                Console.WriteLine("Sending message");
+
+                stream.Write(bytes, 0, bytes.Length);
+
+                byte[] buffer = new byte[100];
+                int numBytesRead = stream.Read(buffer, 0, 100);
+
+                string answer = encoding.GetString(buffer);
+                Console.WriteLine("Answer from server: " + answer);
+
+                Console.WriteLine("Press any key to finish...");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unhandled error:\n" + e);
+            }
+        }
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                //Listen on port 1200. Accept connections from any IP address
+                TcpListener server = new TcpListener(IPAddress.Parse("0.0.0.0"), 1200);
+
+                server.Start();
+
+                Console.WriteLine("Server running and listening on port 1200");
+
+                Socket socket = server.AcceptSocket();
+
+                Console.WriteLine("Connection accepted from " + socket.RemoteEndPoint);
+
+                byte[] buffer = new byte[100];
+
+                socket.Receive(buffer);
+
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                string clientMessage = encoding.GetString(buffer);
+                Console.WriteLine("Message received from client: " + clientMessage);
+
+                socket.Send(encoding.GetBytes("Message received"));
+                Console.WriteLine("ACK sent to client.");
+
+                Task.Delay(2000).Wait();
+
+                socket.Close();
+                server.Stop();
+
+                Console.WriteLine("Server closed. Press any key to finish...");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unhandled error: " + e);
+            }
+        }
+    }
+}
